91f50313edf13c3841649d9784de32295c802644
Hubert Sosiński <sosek108@MacBook-Pro-Hubert.local>
2015-07-05 11:19:36 +0200

    poprawki.

--- js/google-map.js
+++ js/google-map.js
@@ -1,8 +1,8 @@
 function initialize() {
     var mapCanvas = document.getElementById('map-canvas');
     var mapOptions = {
-        center: new google.maps.LatLng(44.5403, -78.5463),
-        zoom: 8,
+        center: new google.maps.LatLng(54.4957509,18.5411908),
+        zoom: 17,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     }
     var map = new google.maps.Map(mapCanvas, mapOptions)
